c792f30a4fb19f10bcf28d54f5cdf44841a161bd
lechinal <alin.lechintan@outlook.com>
2023-08-13 17:25:54 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ export const App = () => {
   return (
     <div
       style={{
-        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
